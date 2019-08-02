6166e3508cc7b1debfee120d128bcb0243f2bc4b
Alexis Josephides <ajosephides@gmail.com>
2019-08-02 17:12:36 +0100

    added another reverse algo that only iterates through half array lengthe and swaps in the existing array

--- reverse.rb
+++ reverse.rb
@@ -20,3 +20,17 @@ end
 def aj_reverse2(array)
   return array.each_with_index{ |value, index| array.insert(0, array.delete_at(index)) }
 end
+
+#iterate through half of the array and swap each element with its mirror
+
+def aj_reverse_3(array)
+  number_of_iterations = (array.length / 2) - 1
+  i = 0
+  while i <= number_of_iterations
+    temp_value_to_move = array[i]
+    array[i] = array[-(i + 1)]
+    array[-(i + 1)] = temp_value_to_move
+    i = i + 1
+  end
+  return array
+end

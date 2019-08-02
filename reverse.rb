d40cec9b9ce7f08e1874331938494dfc5d7ff7fb
Alexis Josephides <ajosephides@gmail.com>
2019-08-02 08:21:08 +0100

    experiment with setting size of array at begining of algo instead of it being dynamic

--- reverse.rb
+++ reverse.rb
@@ -9,6 +9,11 @@ def aj_reverse(array)
   return reversed
 end
 
+def aj_reverse_fixed_array(array)
+  reversed = Array.new(array.length)
+  array.each{ |value| reversed.unshift(value) }
+  return reversed
+end
 
 # iterate through array and take each item and move it to the front of the array
 # no graphs for this as it took and age to run and so stopped it.
